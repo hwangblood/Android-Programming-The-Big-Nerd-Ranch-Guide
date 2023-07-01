fddbf4aec247d795e849717766fcc119b34313b9
hwangblood <hwangblood@gmail.com>
2023-07-02 00:52:06 +0800

    Chapter 2: Interactive User Interfaces

--- GeoQuiz/app/src/main/java/com/hwangblood/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/hwangblood/android/geoquiz/MainActivity.kt
@@ -2,43 +2,65 @@ package com.hwangblood.android.geoquiz
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.hwangblood.android.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view: View ->
-
-            val mySnackbar = Snackbar.make(findViewById(R.id.root),
-                R.string.correct_toast, Snackbar.LENGTH_LONG)
-            mySnackbar.setAction(R.string.ok) {
-                Toast.makeText(
-                    this, R.string.correct_toast, Toast.LENGTH_SHORT
-                ).show()
-            }
-            mySnackbar.show()
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
         }
-        falseButton.setOnClickListener { view: View ->
-            val mySnackbar = Snackbar.make(findViewById(R.id.root),
-                R.string.incorrect_toast, Snackbar.LENGTH_LONG)
-            mySnackbar.setAction(R.string.ok) {
-                Toast.makeText(
-                    this, R.string.incorrect_toast, Toast.LENGTH_SHORT
-                ).show()
-            }
-            mySnackbar.show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
         }
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (correctAnswer == userAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Snackbar.make(
+            binding.root, messageResId, Snackbar.LENGTH_LONG
+        ).setAction(R.string.ok) {
+            Toast.makeText(
+                this, R.string.incorrect_toast, Toast.LENGTH_SHORT
+            ).show()
+        }.show()
     }
 }
---
+++ GeoQuiz/app/src/main/java/com/hwangblood/android/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package com.hwangblood.android.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
+
